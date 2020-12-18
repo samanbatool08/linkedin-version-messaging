66dc2c61ff5db4b3b66963f6c3ee5d249a306f71
Your Name <samanbatool08@gmail.com>
2020-12-18 18:54:10 -0500

    sidebar build

--- linkedin-messaging/src/Sidebar.js
+++ linkedin-messaging/src/Sidebar.js
@@ -1,10 +1,31 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Sidebar.css';
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className='sidebar'>
+            <div className='sidebar__top'>
+                <img src='' alt=''/>
+                <Avatar className='sidebar__avatar'/>
+                <h2>Saman Batool</h2>
+                <h2>samanbatool08@gmail.com</h2>
+            </div>
+
+            <div className='sidebar__stats'>
+                <div className='sidebar__stat'>
+                    <p>Who viewed you</p>
+                    <p className='sidebar__statNumber'>2,543</p>
+                </div>
+                <div className='sidebar__stat'>
+                    <p>Views on post</p>
+                    <p className='sidebar__statNumber'>2,448</p>
+                </div>
+            </div>
+
+            <div className='sidebar__bottom'>
+                <p>Recent</p>
+            </div>
         </div>
     )
 }
