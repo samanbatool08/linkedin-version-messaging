f20a38ad288db70bba8686fddc70789d668cf848
Your Name <samanbatool08@gmail.com>
2020-12-18 15:30:22 -0500

    header sections

--- linkedin-messaging/src/App.js
+++ linkedin-messaging/src/App.js
@@ -5,8 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>biulding linkedin clone </h1>
-
       {/* Header */}
       <Header />
       {/* App body */}
--- linkedin-messaging/src/Header.js
+++ linkedin-messaging/src/Header.js
@@ -5,6 +5,19 @@ function Header() {
     return (
         <div className='header'>
             <h1>this is the header</h1>
+
+            <div classNmae='header__left'>
+                <img src='' alt=''/>
+
+                <div className='header__search'>
+                    {/* SearchIcon from materialui */}
+                    <input type='text'/>
+                </div>
+            </div>
+
+            <div className='header__right'>
+
+            </div>
         </div>
     )
 }
