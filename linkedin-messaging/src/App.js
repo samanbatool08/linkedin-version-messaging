a569b2e4d299656f91a39533e057854b90a68226
Your Name <samanbatool08@gmail.com>
2020-12-18 18:35:06 -0500

    setup sidebar comp

--- linkedin-messaging/src/App.js
+++ linkedin-messaging/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Header from './Header.js';
+import Sidebar from './Sidebar.js';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
       <Header />
       {/* App body */}
+      <div className='app__body'>
         {/* Sidebar */}
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
+
+      </div>
 
     </div>
   );
