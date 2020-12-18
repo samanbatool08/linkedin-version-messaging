875d6a548e136272f551142ceb5b913f93723928
Your Name <samanbatool08@gmail.com>
2020-12-18 15:26:47 -0500

    header component

--- linkedin-messaging/src/App.js
+++ linkedin-messaging/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './Header.js';
 import './App.css';
 
 function App() {
@@ -7,12 +8,12 @@ function App() {
       <h1>biulding linkedin clone </h1>
 
       {/* Header */}
-
+      <Header />
       {/* App body */}
         {/* Sidebar */}
         {/* Feed */}
         {/* Widgets */}
-        
+
     </div>
   );
 }
