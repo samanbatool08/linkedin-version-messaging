fb54c523008cfd919e1297bd096c966ea13d95ca
Your Name <samanbatool08@gmail.com>
2020-12-18 18:17:33 -0500

    icons rendering

--- linkedin-messaging/src/Header.js
+++ linkedin-messaging/src/Header.js
@@ -2,6 +2,11 @@ import React from 'react'
 import './Header.css'
 import HeaderOption from './HeaderOption.js';
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
@@ -19,7 +24,12 @@ function Header() {
             </div>
 
             <div className='header__right'>
-                <HeaderOption />
+                <HeaderOption Icon={HomeIcon} title='Home'/>
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+                <HeaderOption Icon={ChatIcon} title='Messaging'/>
+                <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+                
             </div>
         </div>
     )
--- linkedin-messaging/src/HeaderOption.js
+++ linkedin-messaging/src/HeaderOption.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './HeaderOption.css';
 
-function HeaderOption() {
+
+function HeaderOption({ Icon, title }) {
     return (
         <div className='headerOption'>
-            
+            {Icon && <Icon className='headerOption__icon'/>}
+            <h3 className='headerOption__title'>{title}</h3>
         </div>
     )
 }
