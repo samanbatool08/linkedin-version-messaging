2b36f2f02b3e01ca14e23834eff8929ee60268ee
Your Name <samanbatool08@gmail.com>
2020-12-18 19:43:12 -0500

    sidebar recents sect

--- linkedin-messaging/src/Sidebar.js
+++ linkedin-messaging/src/Sidebar.js
@@ -3,6 +3,14 @@ import React from 'react';
 import './Sidebar.css';
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+            <div className='sidebar__recentItem'>
+                <span className='sidebar__hash'>#</span>
+                <p>{topic}</p>
+            </div>
+    );
+
     return (
         <div className='sidebar'>
             <div className='sidebar__top'>
@@ -10,7 +18,7 @@ function Sidebar() {
                 <img src='https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHww%3D&ww=1000&q=80' alt=''/>
                 <Avatar className='sidebar__avatar'/>
                 <h2>Saman Batool</h2>
-                <h2>samanbatool08@gmail.com</h2>
+                <h4>samanbatool08@gmail.com</h4>
             </div>
 
             <div className='sidebar__stats'>
@@ -26,6 +34,11 @@ function Sidebar() {
 
             <div className='sidebar__bottom'>
                 <p>Recent</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('design')}
+                {recentItem('developer')}
             </div>
         </div>
     )
