c2672c820bc77cab4347b0beec19b65b10625177
Your Name <samanbatool08@gmail.com>
2020-12-18 20:12:52 -0500

    building status/message form

--- linkedin-messaging/src/Feed.js
+++ linkedin-messaging/src/Feed.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Feed.css';
+import CreateIcon from '@material-ui/icons/Create';
 
 function Feed() {
     return (
-        <div>
-            
+        <div className='feed'>
+            <div className='feed__inputContainer'>
+                <div className='feed__input'>
+                    <CreateIcon />
+                    <form>
+                        <input type='text'/>
+                        <button type='submit'>Send</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
