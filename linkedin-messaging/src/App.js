690b2fecdebfd0c5003e1ccd44afa767bc5273b1
Your Name <samanbatool08@gmail.com>
2021-01-02 18:31:37 -0500

    setup store

--- linkedin-messaging/src/App.js
+++ linkedin-messaging/src/App.js
@@ -2,20 +2,24 @@ import React from 'react';
 import Header from './Header.js';
 import Sidebar from './Sidebar.js';
 import Feed from './Feed.js';
+import Login from './Login.js';
 import './App.css';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
-      {/* App body */}
-      <div className='app__body'>
+
+      {!user ? <Login /> 
+      : 
+      ( <div className='app__body'>
         <Sidebar />
         {/* Feed */} 
         <Feed />
         {/* Widgets */}
-
-      </div>
+      </div> )}
 
     </div>
   );
