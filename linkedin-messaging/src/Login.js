67ee4d2dfb9d0839346cf982e4757cd9050941ee
Your Name <samanbatool08@gmail.com>
2021-01-02 19:12:04 -0500

    building signup form

--- linkedin-messaging/src/Login.js
+++ linkedin-messaging/src/Login.js
@@ -4,7 +4,14 @@ import './Login.css';
 function Login() {
     return (
         <div className='login'>
-            <h1>You are not logged in</h1>
+            <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" alt=""/>
+       
+       
+        <form>
+            <input placeholder='Full name (required to sign up)' type='text' />
+            <input placeholder='Profile pic URL (optional)' type='text' />
+
+        </form>
         </div>
     )
 }
